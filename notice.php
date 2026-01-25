8fbaa94dcc1629527182c6662dc793f0178e75bc
ghrasool426-png <ghrasool426@gmail.com>
2026-01-25 20:41:58 +0530

    Create notice.php

---
+++ notice.php
@@ -0,0 +1,24 @@
+<?php
+// Show or hide notice
+$notice_status = "on";
+
+// Notice text (jo red strip me dikhega)
+$notice_text = "🔴 IGNOU Admission 2026 Open – Official Notification";
+
+// OFFICIAL IGNOU website link
+$notice_link = "https://ignouadmission.samarth.edu.
+<?php
+// on = show | off = hide
+$notice_status = "on";
+
+/* NOTICE LIST (2 notices) */
+$notices = [
+  [
+    "text" => "🔴 IGNOU Admission 2026 Open – Official Notification",
+    "link" => "https://ignouadmission.samarth.edu.in/"
+  ],
+  [
+    "text" => "📢 IGNOU Assignment Submission Last Date Extended – Official Notice",
+    "link" => "https://www.ignou.ac.in/"
+  ]
+];in/";
